226a0470327fa17aafd336621307ef449de36002
Marcus Brandão <marcusergue@gmail.com>
2023-10-09 10:02:33 -0300

    updated 09-10-2023

--- src/academy/devdojo/maratonajava/javacore/Lclassesabstratas/dominio/Funcionario.java
+++ src/academy/devdojo/maratonajava/javacore/Lclassesabstratas/dominio/Funcionario.java
@@ -1,4 +1,11 @@
 package academy.devdojo.maratonajava.javacore.Lclassesabstratas.dominio;
 
-public class Funcionario {
+public abstract class Funcionario {
+    protected String nome;
+    protected double salario;
+
+    public Funcionario(String nome, double salario) {
+        this.nome = nome;
+        this.salario = salario;
+    }
 }
