b3e5f084f173383461b53137875161dc01ac7a39
Marcus Brandão <marcusergue@gmail.com>
2023-10-10 11:37:49 -0300

    update aulas 10-10-23

--- src/academy/devdojo/maratonajava/javacore/Npolimorfismo/servico/CalculadoraImposto.java
+++ src/academy/devdojo/maratonajava/javacore/Npolimorfismo/servico/CalculadoraImposto.java
@@ -4,13 +4,13 @@ import academy.devdojo.maratonajava.javacore.Npolimorfismo.dominio.Produto;
 import academy.devdojo.maratonajava.javacore.Npolimorfismo.dominio.Tomate;
 
 public class CalculadoraImposto {
-    public static void calcularImposto(Produto produto){
+    public static void calcularImposto(Produto produto) {
         System.out.println("Relatorio de imposto");
         double imposto = produto.calcularImposto();
-        System.out.println("Produto: "+produto.getNome());
-        System.out.println("Valor: "+produto.getValor());
-        System.out.println("imposto a ser pago: "+imposto);
-        if(produto instanceof Tomate) {
+        System.out.println("Produto: " + produto.getNome());
+        System.out.println("Valor: " + produto.getValor());
+        System.out.println("imposto a ser pago: " + imposto);
+        if (produto instanceof Tomate) {
             String dataValidade = ((Tomate) produto).getDataValidade();
 
 
