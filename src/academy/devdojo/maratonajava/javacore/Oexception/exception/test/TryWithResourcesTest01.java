b7f4ff1a22f3dbd061b980fb6c299801941baca6
Marcus Brandão <marcusergue@gmail.com>
2023-10-13 10:42:15 -0300

    update aulas 13-10-23

---
+++ src/academy/devdojo/maratonajava/javacore/Oexception/exception/test/TryWithResourcesTest01.java
@@ -0,0 +1,43 @@
+package academy.devdojo.maratonajava.javacore.Oexception.exception.test;
+
+import academy.devdojo.maratonajava.javacore.Oexception.exception.dominio.Leitor1;
+import academy.devdojo.maratonajava.javacore.Oexception.exception.dominio.Leitor2;
+
+import java.io.*;
+
+public class TryWithResourcesTest01 {
+    public static void main(String[] args) {
+    lerArquivo();
+    }
+
+    //   try with resources pode ser utilizado sem o catch
+    //    porem deve ser usado o throws e a exceção que está sendo lançada
+
+    public static void lerArquivo() {
+        try (Leitor1 leitor1 = new Leitor1();
+             Leitor2 leitor2 = new Leitor2()) {
+
+        } catch (IOException e) {
+
+        }
+    }
+
+    public static void lerArquivo2() {
+        Reader reader = null;
+        try {
+            reader = new BufferedReader(new FileReader("teste.txt"));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (reader != null) {
+                    reader.close();
+                }
+            } catch (IOException exception) {
+                exception.printStackTrace();
+            }
+        }
+    }
+}
+
+//104 - Exceções pt 10 - Exceção customizada
