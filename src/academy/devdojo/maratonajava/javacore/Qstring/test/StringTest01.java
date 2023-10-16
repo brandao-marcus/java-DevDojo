6a8169077e6112baeb9c1c879b6ccc2b9b9fea85
Marcus Brandão <marcusergue@gmail.com>
2023-10-16 09:26:56 -0300

    update aulas 16-10-23

--- src/academy/devdojo/maratonajava/javacore/Qstring/test/StringTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Qstring/test/StringTest01.java
@@ -7,7 +7,7 @@ public class StringTest01 {
 
         String nome = "Marcus"; // String constant pools.
         String nome2 = "Marcus";
-        nome = nome.concat(" Brandao");
+        nome = nome.concat(" Brandao"); // mesma coisa que: nome += " Brandao"
         System.out.println(nome);
         System.out.println(nome == nome2);
         String nome3 = new String("Marcus"); // 1 variavel de referencia, 2 objeto do tipo string, 3 uma string no pool de string.
