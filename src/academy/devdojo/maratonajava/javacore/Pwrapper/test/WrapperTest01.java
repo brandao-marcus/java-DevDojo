17d7f6f5888ee92d685b721af18c3a67812c5607
Marcus Brandão <marcusergue@gmail.com>
2023-10-16 08:40:52 -0300

    update aulas 13-10-23

---
+++ src/academy/devdojo/maratonajava/javacore/Pwrapper/test/WrapperTest01.java
@@ -0,0 +1,28 @@
+package academy.devdojo.maratonajava.javacore.Pwrapper.test;
+
+public class WrapperTest01 {
+    public static void main(String[] args) {
+        //Tipos primitivos sao relacionados ao tamanho. **
+        byte byteP = 1;
+        short shortP = 1;
+        int intP = 1;
+        long longP = 10;
+        float floatP = 10F;
+        double doubleP = 10D;
+        char charP = 'M';
+        boolean booleanP = false;
+
+        //Classes sendo utilizadas para representar os tipos primitivos.
+        //Wrappers sao relacionadas ao polimorfismo. **
+        Byte byteW = 127;
+        Short shortW = 1;
+        Integer intW = 1;
+        Long longW = 10L;
+        Float floatW = 10F;
+        Double doubleW = 10D;
+        Character charW = 'M';
+        Boolean booleanW = false;
+
+
+    }
+}
