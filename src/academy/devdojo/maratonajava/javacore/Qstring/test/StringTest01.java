e81bcb08ce147f8f475b0384621a95169f1fd4ae
Marcus Brandão <marcusergue@gmail.com>
2023-10-16 09:12:46 -0300

    update aulas 16-10-23

---
+++ src/academy/devdojo/maratonajava/javacore/Qstring/test/StringTest01.java
@@ -0,0 +1,17 @@
+package academy.devdojo.maratonajava.javacore.Qstring.test;
+
+public class StringTest01 {
+    public static void main(String[] args) {
+
+        // String são imutáveis.
+
+        String nome = "Marcus"; // String constant pools.
+        String nome2 = "Marcus";
+        nome = nome.concat(" Brandao");
+        System.out.println(nome);
+        System.out.println(nome == nome2);
+        String nome3 = new String("Marcus"); // 1 variavel de referencia, 2 objeto do tipo string, 3 uma string no pool de string.
+        System.out.println(nome2 == nome3);
+        System.out.println(nome2 == nome3.intern());
+    }
+}
