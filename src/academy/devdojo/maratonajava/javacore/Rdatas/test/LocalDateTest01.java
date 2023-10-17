17e1960278742ed4560a89b23e706b5c542a202a
Marcus Brandão <marcusergue@gmail.com>
2023-10-17 10:33:03 -0300

    update aulas 17-10-23

---
+++ src/academy/devdojo/maratonajava/javacore/Rdatas/test/LocalDateTest01.java
@@ -0,0 +1,32 @@
+package academy.devdojo.maratonajava.javacore.Rdatas.test;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.temporal.ChronoField;
+import java.util.Calendar;
+import java.util.Date;
+
+//LocalDate é uma classe imutável.
+//Não é possível criar instâncias utilizando o new.
+
+
+public class LocalDateTest01 {
+    public static void main(String[] args) {
+        System.out.println(new Date());
+        System.out.println(Calendar.getInstance());
+        LocalDate date = LocalDate.of(2023, Month.SEPTEMBER,24);
+        LocalDate agora = LocalDate.now();
+        System.out.println(date.getYear());
+        System.out.println(date.getMonth());
+        System.out.println(date.getMonthValue());
+        System.out.println(date.getDayOfWeek());
+        System.out.println(date.getDayOfMonth());
+        System.out.println(date.lengthOfMonth());
+        System.out.println(date.get(ChronoField.YEAR));
+        System.out.println(date.get(ChronoField.DAY_OF_MONTH));
+        System.out.println(date);
+        System.out.println(agora);
+        System.out.println(LocalDate.MAX);
+        System.out.println(LocalDate.MIN);
+    }
+}
