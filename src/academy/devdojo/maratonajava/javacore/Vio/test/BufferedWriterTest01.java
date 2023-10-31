99923f1c4b5e43c09dac03f864aa5e4888bf891e
Marcus Brandão <marcusergue@gmail.com>
2023-10-31 10:56:06 -0300

    update aulas 31-10-23

--- src/academy/devdojo/maratonajava/javacore/Vio/test/BufferedWriterTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Vio/test/BufferedWriterTest01.java
@@ -9,10 +9,10 @@ public class BufferedWriterTest01 {
     public static void main(String[] args) {
         File file = new File("file.txt");
         try(FileWriter fw = new FileWriter(file,true);
-            BufferedWriter br = new BufferedWriter(fw)) {
-            br.write("O DevDojo e o melhor curso do Brasil!!");
-            br.newLine(); // Baseado no sistema operacional
-            br.flush();
+            BufferedWriter bw = new BufferedWriter(fw)) {
+            bw.write("O DevDojo e o melhor curso do Brasil!!");
+            bw.newLine(); // Baseado no sistema operacional
+            bw.flush();
         } catch (IOException ex) {
             ex.printStackTrace();
         }
