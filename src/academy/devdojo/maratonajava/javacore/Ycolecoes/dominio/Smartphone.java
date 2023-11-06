b963610f6b51c2d157760b9423f8c7a53e80075f
Marcus Brandão <marcusergue@gmail.com>
2023-11-06 09:11:37 -0300

    update aulas 06-11-23

--- src/academy/devdojo/maratonajava/javacore/Ycolecoes/dominio/Smartphone.java
+++ src/academy/devdojo/maratonajava/javacore/Ycolecoes/dominio/Smartphone.java
@@ -9,6 +9,21 @@ public class Smartphone {
         this.marca = marca;
     }
 
+    // Reflexivo: x.equals(x) = tem q ser true para tudo for diferente de null
+    // Simetrico: para x e y diferentes de null, se x.equals(y) == true logo, y.equals(x) == true
+    // Transitivo: para x,y,z diferentes de null, se x.equals(y) == true, e x.equals(z) == true logo, y.equals(z) == true
+    // Consistente: x.equals(x) sempre retorna true se x for diferente de null
+    // para x diferente de null, x.equals(null) tem q retornar falso.
+
+    @Override
+    public boolean equals(Object obj) {
+        if(obj == null) return false;
+        if(this == obj) return true;
+        if(this.getClass() != obj.getClass()) return false;
+        Smartphone smartphone = (Smartphone) obj;
+        return serialNumber != null && serialNumber.equals(smartphone.serialNumber);
+    }
+
     public String getSerialNumber() {
         return serialNumber;
     }
