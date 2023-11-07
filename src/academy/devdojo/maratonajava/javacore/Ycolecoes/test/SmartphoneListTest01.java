1422726a9638b8c8d160081b6f2c05b8a71593d0
Marcus Brandão <marcusergue@gmail.com>
2023-11-07 10:14:45 -0300

    update aulas 07-11-23

--- src/academy/devdojo/maratonajava/javacore/Ycolecoes/test/SmartphoneListTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Ycolecoes/test/SmartphoneListTest01.java
@@ -15,6 +15,7 @@ public class SmartphoneListTest01 {
         smartphones.add(s2);
         smartphones.add(0, s3);
 
+
         for(Smartphone smartphone : smartphones){
             System.out.println(smartphone);
         }
