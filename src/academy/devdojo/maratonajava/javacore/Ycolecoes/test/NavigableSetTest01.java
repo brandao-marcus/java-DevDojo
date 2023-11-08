e80793af84c85fcc6f4197258e7975e62d2d16b9
Marcus Brandão <marcusergue@gmail.com>
2023-11-08 10:23:12 -0300

    update aulas 08-11-23

--- src/academy/devdojo/maratonajava/javacore/Ycolecoes/test/NavigableSetTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Ycolecoes/test/NavigableSetTest01.java
@@ -14,12 +14,22 @@ class SmartphoneMarcaComparator implements Comparator<Smartphone> {
         return o1.getMarca().compareTo(o2.getMarca());
     }
 }
+
+class MangaPrecoComparator implements Comparator<Manga> {
+
+    @Override
+    public int compare(Manga o1, Manga o2) {
+        return Double.compare(o1.getPreco(), o2.getPreco());
+    }
+}
+
 public class NavigableSetTest01 {
     public static void main(String[] args) {
+        // TreeSet também não armazena elementos duplicados na lista
         NavigableSet<Smartphone> set = new TreeSet<>(new SmartphoneMarcaComparator());
         Smartphone smartphone = new Smartphone("123", "Nokia");
         set.add(smartphone);
-        NavigableSet<Manga> mangas = new TreeSet<>();
+        NavigableSet<Manga> mangas = new TreeSet<>(new MangaPrecoComparator());
         mangas.add(new Manga(5L, "Hellsing Ultimate", 19.9, 0));
         mangas.add(new Manga(1L, "Berserk", 9.5, 5));
         mangas.add(new Manga(4L, "Pokemon", 3.2, 0));
@@ -32,6 +42,22 @@ public class NavigableSetTest01 {
             System.out.println(manga);
         }
 
+        Manga yuyu = new Manga(1L, "Yuyu Hakusho", 3.2, 5);
 
+        // lower: " < " vai trazer o menor valor
+        // floor: " <= " vai trazer o mesmo valor ou o menor antes dele
+        // higher: " > " vai trazer o maior valor
+        // ceiling: " >= " vai trazer o mesmo valor ou o maior apos ele
+
+        System.out.println("-----------------");
+
+        System.out.println(mangas.lower(yuyu));
+        System.out.println(mangas.floor(yuyu));
+        System.out.println(mangas.higher(yuyu));
+        System.out.println(mangas.ceiling(yuyu));
+
+        System.out.println(mangas.size());
+        System.out.println(mangas.pollLast());
+        System.out.println(mangas.size());
     }
 }
