50f1a1e207b7d5c3579fd4f6c6204b4bdc98f662
Marcus Brandão <marcusergue@gmail.com>
2023-11-16 09:39:16 -0300

    update aulas 16-11-23

--- src/academy/devdojo/maratonajava/javacore/ZZBcomportamento/interfaces/CarPredicate.java
+++ src/academy/devdojo/maratonajava/javacore/ZZBcomportamento/interfaces/CarPredicate.java
@@ -2,6 +2,14 @@ package academy.devdojo.maratonajava.javacore.ZZBcomportamento.interfaces;
 
 import academy.devdojo.maratonajava.javacore.ZZBcomportamento.dominio.Car;
 
+@FunctionalInterface
 public interface CarPredicate {
+    // anônimas, funções, conciso
     boolean test(Car car);
+    //(parametro) -> <expressão>
+    //(Car car) -> car.getColor().equals.("green"); >> vai retornar boolean: true or false <<
 }
+
+// Para utilizar lambdas é preciso uma interface funcional = apenas um metodo abstrato
+// O proprio metodo da interface diz como a lambda vai se comportar(contrato)
+  // ex -> boolean test(Car car) = deve retornar um boolean e recebe um parametro que e o carro
