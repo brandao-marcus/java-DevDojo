e29502deaec8e95ae7ed5c1681692712ef655ae3
Marcus Brandão <marcusergue@gmail.com>
2023-12-07 11:02:08 -0300

    update aulas 07-12-23

---
+++ src/academy/devdojo/maratonajava/javacore/ZZHpadroesdeprojeto/dominio/AirCraftSingletonEager.java
@@ -0,0 +1,28 @@
+package academy.devdojo.maratonajava.javacore.ZZHpadroesdeprojeto.dominio;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public final class AirCraftSingletonEager {
+    // Eager initialization
+    private static final AirCraftSingletonEager INSTANCE = new AirCraftSingletonEager("787-900");
+    private final Set<String> availableSeats = new HashSet<>();
+    private final String name;
+
+    private AirCraftSingletonEager(String name) {
+        this.name = name;
+    }
+
+    {
+        availableSeats.add("1A");
+        availableSeats.add("2B");
+    }
+
+    public static AirCraftSingletonEager getINSTANCE() {
+        return INSTANCE;
+    }
+
+    public boolean bookSeat(String seat) {
+        return availableSeats.remove(seat);
+    }
+}
