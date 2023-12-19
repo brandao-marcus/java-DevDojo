3f53e295a6c6a81e5971422d1318e578d79eeb80
Marcus Brandão <marcusergue@gmail.com>
2023-12-19 10:22:28 -0300

    update aulas 19-12-23

--- src/academy/devdojo/maratonajava/javacore/ZZIjdbc/test/ConnectionFactoryTest02.java
+++ src/academy/devdojo/maratonajava/javacore/ZZIjdbc/test/ConnectionFactoryTest02.java
@@ -9,7 +9,7 @@ import java.util.List;
 @Log4j2
 public class ConnectionFactoryTest02 {
     public static void main(String[] args) {
-        Producer producerToUpdate = Producer.builder().id(5).name("MAD").build();
+        Producer producerToUpdate = Producer.builder().id(1).name("MAD").build();
         ProducerServiceRowSet.updateJdbcRowSet(producerToUpdate);
         log.info("-----------------------------------");
         List<Producer> producers = ProducerServiceRowSet.findByNameJdbcRowSet("");
